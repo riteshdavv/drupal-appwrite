4e7266518d02eb55abc9485512af101b53dd8892
Ritesh Singh <ritesh.davv@gmail.com>
2025-06-06 01:19:30 +0530

    Update Login Block

--- web/modules/custom/appwrite_integration/src/Plugin/Block/AppwriteLoginBlock.php
+++ web/modules/custom/appwrite_integration/src/Plugin/Block/AppwriteLoginBlock.php
@@ -18,18 +18,13 @@ class AppwriteLoginBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-    $appwrite_endpoint = 'https://fra.cloud.appwrite.io/v1';
-    $project_id = '683ea5970037a0cd8c8b'; // Replace with actual Appwrite project ID
-    $success_url = 'http://drupal-appwrite.ddev.site/appwrite/bridge';
-    $failure_url = 'http://drupal-appwrite.ddev.site/appwrite/failure';
-
-    $login_url = "{$appwrite_endpoint}/account/sessions/oauth2/github?project={$project_id}&success={$success_url}&failure={$failure_url}&token=true";
+    $login_url = "https://drupal-appwrite.ddev.site/appwrite/login";
 
     return [
       '#markup' => 
         '<a href="' . $login_url . '"><button class="button">Login with GitHub</button></a>',
       '#attached' => [
-        'library' => [], // You can attach a custom CSS/JS library if needed
+        'library' => [],
       ],
     ];
   }
